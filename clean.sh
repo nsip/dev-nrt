3b0ed8640fba1fda806b20b19677c5c351e0d1ea
Qing Miao <digisanQM@outlook.com>
2021-04-08 16:47:38 +1000

    * clean.sh, add removing windows exe

--- clean.sh
+++ clean.sh
@@ -7,3 +7,6 @@ rm -f NRT-*-v*.*.*.zip
 
 # delete all binary files
 find . -type f -executable -exec sh -c "file -i '{}' | grep -q 'x-executable; charset=binary'" \; -print | xargs rm -f
+
+# delete windows executables
+for f in $(find ./ -name '*.log' -or -name '*.exe'); do rm $f; done 
