4211eefec69e5ce530d92b7b8134218e02c4ebe8
Qing Miao <digisanQM@outlook.com>
2021-04-08 17:16:04 +1000

    * clean.sh

--- clean.sh
+++ clean.sh
@@ -5,8 +5,14 @@ set -e
 rm -rf ./build
 rm -f NRT-*-v*.*.*.zip
 
-# delete all binary files
+# delete all Linux binary files
 find . -type f -executable -exec sh -c "file -i '{}' | grep -q 'x-executable; charset=binary'" \; -print | xargs rm -f
 
+# delete all Mac binary files
+find . -type f -executable -exec sh -c "file -i '{}' | grep -q 'x-mach-binary; charset=binary'" \; -print | xargs rm -f
+
 # delete windows executables
-for f in $(find ./ -name '*.log' -or -name '*.exe'); do rm $f; done 
+find . -type f -executable -exec sh -c "file -i '{}' | grep -q 'x-dosexec; charset=binary'" \; -print | xargs rm -f
+
+# delete others
+for f in $(find ./ -name '*.log' -or -name '*.doc'); do rm $f; done
